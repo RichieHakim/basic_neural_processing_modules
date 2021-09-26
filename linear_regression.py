David suggested fix off by one error in conv1d_numba

linear_regression.py
@@ -44,7 +44,7 @@ def LinearRegression_sweep(X_in,
         alphas (ndarray): 
             Alpha parameters. Sets strength of regularization.
         l1_ratios (ndarray): 
-            L1 to L2 ratio parameters.
+            L1 to L2 ratio parameters. 0:L2 only, 1:L1 only
         cv_idx (list of lists of 1-D arrays): 
             Cross-validation indices. Each entry in the outer list is
             for a different run. Each entry in the outer list should 
@@ -70,6 +70,7 @@ def LinearRegression_sweep(X_in,
         compute_preds (bool):
             If True, will compute the predictions for the model.
              preds = theta @ X.T + intercept
+            This is currently done on CPU so it is very slow.
         verbose (int 0-2):
             Preference of whether to print reconstruction scores.
             0: Print nothing
@@ -100,15 +101,19 @@ def LinearRegression_sweep(X_in,
 
     Returns:
         theta (ndarray):
-            Regression coefficients
+            Regression coefficients.
+            Shape: (n_y, n_splits, n_rolls, n_alphas, n_l1Ratios, X.shape[1])
         intercept (ndarray):
             Intercept values
-        R_test (ndarray):
+            Shape: (n_y, n_splits, n_rolls, n_alphas, n_l1Ratios)
+        EV_test (ndarray):
             Pearson correlation scores between test data and reconstructed
             test data.
-        R_train (ndarray):
+            Shape: (n_y, n_splits, n_rolls, n_alphas, n_l1Ratios)
+        EV_train (ndarray):
             Pearson correlation scores between train data and reconstructed
             train data.
+            Shape: (n_y, n_splits, n_rolls, n_alphas, n_l1Ratios)
 
     ============ DEMO ===============
 
@@ -293,7 +298,10 @@ def LinearRegression_sweep(X_in,
                         EV_test[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio] = EV_test_tmp       
                         
                         if compute_preds:
-                            preds[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio] = (theta[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio] @ X_in.T) + intercept[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio]
+                            if method_package=='cuml':
+                                preds[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio] = ((clf.coef_ @ X.T) + clf.intercept_).get()
+                            else:
+                                preds[iter_factor, iter_cv, iter_roll, iter_alpha, iter_l1Ratio] = ((clf.coef_ @ X.T) + clf.intercept_)
                         else:
                             preds = np.nan
 

timeSeries.py
@@ -632,7 +632,7 @@ def conv2d(X, k_rev):
     y.fill(np.nan)
     k_hs = k_rev.size//2
     for ii in prange(X.shape[0]):
-        for i in prange( k_hs , X.shape[1]-(k_hs+1) ):
+        for i in prange( k_hs , X.shape[1]-(k_hs) ):
             y[ii, i] = np.dot(X[ii, 0+i-k_hs : 1+i+k_hs], k_rev)
     return y
 def convolve_numba(X, k, axis=1):
@@ -677,7 +677,7 @@ def conv1d_numba(X, k):
     y = np.empty_like(X)
     y.fill(np.nan)
     k_hs = k.size//2
-    for ii in prange( k_hs , len(X)-(k_hs+1) ):
+    for ii in prange( k_hs , len(X)-(k_hs) ):
         y[ii] = np.dot(X[0+ii-k_hs : 1+ii+k_hs], k)
     return y
     