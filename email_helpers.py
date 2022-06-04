fbeb307a1a79bfe1ebf727d939d75621b870f0e4
RichieHakim <RichHakim@gmail.com>
2022-06-04 05:11:06 -0400

    new email module

---
+++ email_helpers.py
@@ -0,0 +1,62 @@
+import sendgrid
+from sendgrid.helpers.mail import To, From, Content, Mail
+
+
+class Sender():
+    def __init__(
+        self,
+        api_key='***REMOVED***',
+        ):
+        """
+        Initialize the Sender object.
+        RH 2022
+        
+        Args:
+            api_key (str):
+                The SendGrid API key.
+                Get your API key at https://app.sendgrid.com/settings/api_keys.
+        """
+        self.sg = sendgrid.SendGridAPIClient(api_key=api_key)
+
+    def send(
+        self, 
+        from_email='serverteeny@gmail.com', 
+        to_emails=['serverteeny@gmail.com',], 
+        subject='Subject', 
+        content='Body',
+        verbose=False,
+        ):
+        """
+        Send an email.
+        RH 2022
+
+        Args:
+            from_email (str):
+                The email address of the sender.
+            to_emails (list of str):
+                The email addresses of the recipients.
+            subject (str):
+                The subject of the email.
+            content (str):
+                The content of the email.
+            verbose (bool):
+                Whether to print the response from SendGrid.
+
+        Returns:
+            The response from SendGrid.
+        """
+        self.from_email = From(from_email)
+        self.to_emails = To(to_emails)
+        self.subject = subject
+        self.content = Content("text/plain", content)
+        
+        self.mail = Mail(self.from_email, self.to_emails, self.subject, self.content)
+        
+        self.response = self.sg.client.mail.send.post(request_body=self.mail.get())
+
+        if verbose:
+            print(self.response.status_code)
+            print(self.response.body)
+            print(self.response.headers)
+
+        return self.response
