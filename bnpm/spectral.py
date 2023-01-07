f095a67473f618cf4837b3430b6115b5ed176d29
RichieHakim <RichHakim@gmail.com>
2023-01-07 17:41:26 -0500

    update VQT to be like face rhythms

--- bnpm/spectral.py
+++ bnpm/spectral.py
@@ -417,6 +417,7 @@ class VQT():
         n_freq_bins=55,
         win_size=501,
         downsample_factor=4,
+        padding='valid',
         DEVICE_compute='cpu',
         DEVICE_return='cpu',
         batch_size=1000,
@@ -424,7 +425,7 @@ class VQT():
         filters=None,
         plot_pref=False,
         progressBar=True,
-        ):
+    ):
         """
         Variable Q Transform.
         Class for applying the variable Q transform to signals.
@@ -464,6 +465,12 @@ class VQT():
                 If the length of the input signal is not
                  divisible by downsample_factor, the signal
                  will be zero-padded at the end so that it is.
+            padding (str):
+                Padding to use for the signal.
+                'same' will pad the signal so that the output
+                 signal is the same length as the input signal.
+                'valid' will not pad the signal. So the output
+                 signal will be shorter than the input signal.
             DEVICE_compute (str):
                 Device to use for computation.
             DEVICE_return (str):
@@ -482,11 +489,17 @@ class VQT():
                 shape: (n_freq_bins, win_size)
             plot_pref (bool):
                 Whether to plot the filters.
+            progressBar (bool):
+                Whether to show a progress bar.
         """
-
+        ## Prepare filters
         if filters is not None:
+            ## Use provided filters
+            self.using_custom_filters = True
             self.filters = filters
         else:
+            ## Make new filters
+            self.using_custom_filters = False
             self.filters, self.freqs, self.wins = make_VQT_filters(
                 Fs_sample=Fs_sample,
                 Q_lowF=Q_lowF,
@@ -497,29 +510,10 @@ class VQT():
                 win_size=win_size,
                 plot_pref=plot_pref,
             )
-
-        ## Fill in missing parameters
-        args_default = {
-            'Fs_sample': 1000,
-            'Q_lowF': 3,
-            'Q_highF': 20,
-            'F_min': 10,
-            'F_max': 400,
-            'n_freq_bins': 55,
-            'win_size': 501,
-            'downsample_factor': 4,
-            'DEVICE_compute': 'cpu',
-            'DEVICE_return': 'cpu',
-            'batch_size': 1000,
-            'return_complex': False,
-            'filters': None,
-            'plot_pref': False,
-            'progressBar': True,            
-        }
-
-        ## if the parameter was passed as an arg then place it in the args dict, otherwise use the default
-        self.args = {key: locals()[key] if key in locals() else val for key, val in args_default.items()}
-
+        ## Gather parameters from arguments
+        self.Fs_sample, self.Q_lowF, self.Q_highF, self.F_min, self.F_max, self.n_freq_bins, self.win_size, self.downsample_factor, self.padding, self.DEVICE_compute, \
+            self.DEVICE_return, self.batch_size, self.return_complex, self.plot_pref, self.progressBar = \
+                Fs_sample, Q_lowF, Q_highF, F_min, F_max, n_freq_bins, win_size, downsample_factor, padding, DEVICE_compute, DEVICE_return, batch_size, return_complex, plot_pref, progressBar
 
     def _helper_ds(self, X: torch.Tensor, ds_factor: int=4, return_complex: bool=False):
         if ds_factor == 1:
@@ -536,8 +530,8 @@ class VQT():
 
     def _helper_conv(self, arr, filters, take_abs, DEVICE):
         out = torch.complex(
-            torch.nn.functional.conv1d(arr.to(DEVICE)[:,None,:],  torch.real(filters.T).to(DEVICE).T[:,None,:], padding='same'),
-            torch.nn.functional.conv1d(arr.to(DEVICE)[:,None,:], -torch.imag(filters.T).to(DEVICE).T[:,None,:], padding='same')
+            torch.nn.functional.conv1d(arr.to(DEVICE)[:,None,:],  torch.real(filters.T).to(DEVICE).T[:,None,:], padding=self.padding),
+            torch.nn.functional.conv1d(arr.to(DEVICE)[:,None,:], -torch.imag(filters.T).to(DEVICE).T[:,None,:], padding=self.padding)
         )
         if take_abs:
             return torch.abs(out)
@@ -557,24 +551,46 @@ class VQT():
             Spectrogram (Torch tensor):
                 Spectrogram of the input signal.
                 shape: (n_channels, n_samples_ds, n_freq_bins)
+            x_axis (Torch tensor):
+                New x-axis for the spectrogram in units of samples.
+                Get units of time by dividing by self.Fs_sample.
+            self.freqs (Torch tensor):
+                Frequencies of the spectrogram.
         """
         if type(X) is not torch.Tensor:
-            X = torch.as_tensor(X, dtype=torch.float32, device=self.args['DEVICE_compute'])
+            X = torch.as_tensor(X, dtype=torch.float32, device=self.DEVICE_compute)
 
         if X.ndim==1:
             X = X[None,:]
 
-        batches = indexing.make_batches(X, batch_size=self.args['batch_size'], length=X.shape[0])
+        ## Make iterator for batches
+        batches = indexing.make_batches(X, batch_size=self.batch_size, length=X.shape[0])
 
-        test = [self._helper_ds(
+        ## Make spectrograms
+        specs = [self._helper_ds(
             X=self._helper_conv(
                 arr=arr, 
                 filters=self.filters, 
-                take_abs=(self.args['return_complex']==False),
-                DEVICE=self.args['DEVICE_compute']
+                take_abs=(self.return_complex==False),
+                DEVICE=self.DEVICE_compute
                 ), 
-            ds_factor=self.args['downsample_factor'],
-            return_complex=self.args['return_complex'],
-            ).to(self.args['DEVICE_return']) for arr in tqdm(batches, disable=(self.args['progressBar']==False), leave=True, total=int(np.ceil(X.shape[0]/self.args['batch_size'])))]
+            ds_factor=self.downsample_factor,
+            return_complex=self.return_complex,
+            ).to(self.DEVICE_return) for arr in tqdm(batches, disable=(self.progressBar==False), leave=True, total=int(np.ceil(X.shape[0]/self.batch_size)))]
+        specs = torch.cat(specs, dim=0)
+
+        ## Make x_axis
+        x_axis = torch.nn.functional.avg_pool1d(
+            torch.arange(0, X.shape[-1])[None,:], 
+            kernel_size=[int(self.downsample_factor)], 
+            stride=self.downsample_factor, ceil_mode=True
+            ).squeeze()
         
-        return torch.cat(test, dim=0)
+        return specs, x_axis, self.freqs
+
+    def __repr__(self):
+        if self.using_custom_filters:
+            return f"VQT with custom filters"
+        else:
+            return f"VQT object with parameters: Fs_sample={self.Fs_sample}, Q_lowF={self.Q_lowF}, Q_highF={self.Q_highF}, F_min={self.F_min}, F_max={self.F_max}, n_freq_bins={self.n_freq_bins}, win_size={self.win_size}, downsample_factor={self.downsample_factor}, DEVICE_compute={self.DEVICE_compute}, DEVICE_return={self.DEVICE_return}, batch_size={self.batch_size}, return_complex={self.return_complex}, plot_pref={self.plot_pref}"
+
