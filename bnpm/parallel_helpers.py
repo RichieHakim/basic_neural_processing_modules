54d208d25c2c7a3fe7d467fa911181aee995f5cd
RichieHakim <RichHakim@gmail.com>
2023-01-24 14:23:07 -0500

    joblib not working well commented out

--- bnpm/parallel_helpers.py
+++ bnpm/parallel_helpers.py
@@ -14,12 +14,17 @@ def map_parallel(func, args, method='multithreading', workers=-1, prog_bar=True)
             Function to map.
         args (list):
             List of arguments to map the function to.
+            len(args) should be equal to the number of arguments.
+            If the function takes multiple arguments, args should be an
+             iterable (e.g. list, tuple, generator) of length equal to
+             the number of arguments. Each element can then be an iterable
+             for each iteration of the function.
         method (str):
             Method to use for parallelization. Options are:
                 'multithreading': Use multithreading from concurrent.futures.
                 'multiprocessing': Use multiprocessing from concurrent.futures.
                 'mpire': Use mpire
-                'joblib': Use joblib.Parallel
+                # 'joblib': Use joblib.Parallel
                 'serial': Use list comprehension
         workers (int):
             Number of workers to use. If -1, use all available.
@@ -33,6 +38,9 @@ def map_parallel(func, args, method='multithreading', workers=-1, prog_bar=True)
     if workers == -1:
         workers = mp.cpu_count()
 
+    ## Get number of arguments. If args is a generator, make None.
+    n_args = len(args[0]) if hasattr(args, '__len__') else None
+
     if method == 'multithreading':
         executor = ThreadPoolExecutor
     elif method == 'multiprocessing':
@@ -40,16 +48,17 @@ def map_parallel(func, args, method='multithreading', workers=-1, prog_bar=True)
     elif method == 'mpire':
         import mpire
         executor = mpire.WorkerPool
-    elif method == 'joblib':
-        import joblib
-        return joblib.Parallel(n_jobs=workers)(joblib.delayed(func)(arg) for arg in args)
+    # elif method == 'joblib':
+    #     import joblib
+    #     return joblib.Parallel(n_jobs=workers)(joblib.delayed(func)(arg) for arg in tqdm(args, total=n_args, disable=prog_bar!=True))
     elif method == 'serial':
-        return [func(arg) for arg in tqdm(args, disable=prog_bar!=True)]
+        # return [func(*arg) for arg in tqdm(args, disable=prog_bar!=True)]
+        return list(tqdm(map(func, *args), total=n_args, disable=prog_bar!=True))
     else:
         raise ValueError(f"method {method} not recognized")
 
     with executor(workers) as ex:
-        return list(tqdm(ex.map(func, args), total=len(args), disable=prog_bar!=True))
+        return list(tqdm(ex.map(func, *args), total=n_args, disable=prog_bar!=True))
     
 
 def multiprocessing_pool_along_axis(x_in, function, n_workers=None, axis=0, **kwargs):
