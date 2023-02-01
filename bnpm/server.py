2e0b2f27a9b4ccaab943f317e987dbd41a00dd57
RichieHakim <RichHakim@gmail.com>
2023-02-01 16:17:32 -0500

    new sftp get method that uses a new downloader class

--- bnpm/server.py
+++ bnpm/server.py
@@ -3,6 +3,8 @@ import os
 import json
 import copy
 import time
+import typing
+import sys
 
 from . import container_helpers
 
@@ -532,6 +534,7 @@ class sftp_interface():
         """
         if ssh_client is None:
             self.transport = paramiko.Transport((hostname, port))  ## open a transport object
+            self.sftp = paramiko.SFTPClient.from_transport(self.transport)
         else:
             if isinstance(ssh_client, ssh_interface):
                 client = ssh_client.client
@@ -795,11 +798,200 @@ class sftp_interface():
         except FileNotFoundError:
             return False
 
+    def get(
+        self, 
+        remotepath, 
+        localpath, 
+        mkdirs=True,
+        prefetch=False, 
+        callback=None, 
+        prog_bar=True,
+        ):
+        """
+        Get a file from the remote host.
+        Uses a fancy class to do the downloading. It doesn't slow
+         down due to the size of the file.
+        Some extra stuff in here like a progress bar.
+        RH 2022
 
+        Args:
+            remote_path (str):
+                Path to the file on the remote host.
+            local_path (str):
+                Path to the file on the local host.
+            prefetch (bool):
+                Whether or not to prefetch the file.
+            callback (function):
+                Callback function to call within self.sftp.get
+            prog_bar (bool):
+                Whether or not to show a progress bar.
+        
+        Returns:
+            None
+        """
+        Path(localpath).parent.mkdir(parents=True, exist_ok=True) if mkdirs else None
+
+        with _TqdmWrap(ascii=False, unit='b', unit_scale=True) as pbar:
+            def conj_func(a,b):
+                pbar.viewBar(a, b)
+                if callback is not None:
+                    callback(a, b)
+                    
+            with self.sftp.open(remotepath, 'rb') as f_in, open(localpath, 'wb') as f_out:
+                _SFTPFileDownloader(
+                    f_in=f_in,
+                    f_out=f_out,
+                    prefetch=prefetch,
+                    callback=conj_func if prog_bar else callback,
+                ).download()
 
     def close(self):
         self.sftp.close()
         self.transport.close()
+
+
+"""
+Implemented by casperdcl here: https://github.com/tqdm/tqdm/issues/311#issuecomment-387066847
+"""
+try:
+    from tqdm import tqdm
+except ImportError:
+    class _TqdmWrap(object):
+        # tqdm not installed - construct and return dummy/basic versions
+        def __init__(self, *a, **k):
+            pass
+
+        def viewBar(self, a, b):
+            # original version
+            res = a / int(b) * 100
+            sys.stdout.write('\rComplete precent: %.2f %%' % (res))
+            sys.stdout.flush()
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *a):
+            return False
+else:
+    class _TqdmWrap(tqdm):
+        def viewBar(self, a, b):
+            self.total = int(b)
+            self.update(int(a - self.n))  # update pbar with increment
+
+
+class _SFTPFileDownloader:
+    """
+    Helper class to download large file with paramiko sftp client with limited number of concurrent requests.
+    Implemented by vznncv here: https://gist.github.com/vznncv/cb454c21d901438cc228916fbe6f070f
+    """
+
+    _DOWNLOAD_MAX_REQUESTS = 48
+    _DOWNLOAD_MAX_CHUNK_SIZE = 0x8000
+
+    def __init__(self, f_in: paramiko.SFTPFile, f_out: typing.BinaryIO, callback=None, prefetch=False):
+        self.f_in = f_in
+        self.f_out = f_out
+        self.callback = callback
+        self.prefetch = prefetch
+
+        self.requested_chunks = {}
+        self.received_chunks = {}
+        self.saved_exception = None
+
+    def download(self):
+        file_size = self.f_in.stat().st_size
+        requested_size = 0
+        received_size = 0
+
+        while True:
+            # send read requests
+            while len(self.requested_chunks) + len(self.received_chunks) < self._DOWNLOAD_MAX_REQUESTS and \
+                    requested_size < file_size:
+                chunk_size = min(self._DOWNLOAD_MAX_CHUNK_SIZE, file_size - requested_size)
+                request_id = self._sftp_async_read_request(
+                    fileobj=self,
+                    file_handle=self.f_in.handle,
+                    offset=requested_size,
+                    size=chunk_size
+                )
+                self.requested_chunks[request_id] = (requested_size, chunk_size)
+                requested_size += chunk_size
+
+            # receive blocks if they are available
+            # note: the _async_response is invoked
+            self.f_in.sftp._read_response()
+            self._check_exception()
+
+            # write received data to output stream
+            while True:
+                chunk = self.received_chunks.pop(received_size, None)
+                if chunk is None:
+                    break
+                _, chunk_size, chunk_data = chunk
+                self.f_out.write(chunk_data)
+                if self.callback is not None:
+                    self.callback(received_size, file_size)
+
+                received_size += chunk_size
+
+            # check transfer status
+            if received_size >= file_size:
+                break
+
+            # check chunks queues
+            if not self.requested_chunks and len(self.received_chunks) >= self._DOWNLOAD_MAX_REQUESTS:
+                raise ValueError("SFTP communication error. The queue with requested file chunks is empty and"
+                                 "the received chunks queue is full and cannot be consumed.")
+
+        return received_size
+
+    def _sftp_async_read_request(self, fileobj, file_handle, offset, size):
+        sftp_client = self.f_in.sftp
+
+        with sftp_client._lock:
+            num = sftp_client.request_number
+
+            msg = paramiko.Message()
+            msg.add_int(num)
+            msg.add_string(file_handle)
+            msg.add_int64(offset)
+            msg.add_int(size)
+
+            sftp_client._expecting[num] = fileobj
+            sftp_client.request_number += 1
+
+        sftp_client._send_packet(paramiko.sftp.CMD_READ, msg)
+        return num
+
+    def _async_response(self, t, msg, num):
+        if t == paramiko.sftp.CMD_STATUS:
+            # save exception and re-raise it on next file operation
+            try:
+                self.f_in.sftp._convert_status(msg)
+            except Exception as e:
+                self.saved_exception = e
+            return
+        if t != paramiko.sftp.CMD_DATA:
+            raise paramiko.SFTPError("Expected data")
+        data = msg.get_string()
+
+        chunk_data = self.requested_chunks.pop(num, None)
+        if chunk_data is None:
+            return
+
+        # save chunk
+        offset, size = chunk_data
+
+        if size != len(data):
+            raise paramiko.SFTPError(f"Invalid data block size. Expected {size} bytes, but it has {len(data)} size")
+        self.received_chunks[offset] = (offset, size, data)
+
+    def _check_exception(self):
+        """if there's a saved exception, raise & clear it"""
+        if self.saved_exception is not None:
+            x = self.saved_exception
+            self.saved_exception = None
+            raise x
 
 
 def pw_encode(pw):
