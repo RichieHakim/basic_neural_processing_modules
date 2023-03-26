c6e7a6ceb1830d9dc7fa14120eecc33aebe7ff4c
RichieHakim <RichHakim@gmail.com>
2023-03-25 22:09:02 -0400

    added get_nd_butterworth_filter function

--- bnpm/other_peoples_code.py
+++ bnpm/other_peoples_code.py
@@ -149,3 +149,65 @@ class ReducedRankRidge(sklearn.base.MultiOutputMixin, sklearn.base.RegressorMixi
             self.intercept_ = np.zeros(y.shape[1])
         return self
 
+
+
+
+###############################################################################################################
+############################################ FROM SKIMAGE #####################################################
+###############################################################################################################
+
+import functools
+
+import numpy as np
+import scipy.fft
+
+# from .._shared.utils import _supported_float_type
+
+
+## Fixed the indexing because the skimage nerds drink too much coffee. RH.
+def get_nd_butterworth_filter(shape, factor, order, high_pass, real,
+                               dtype=np.float64, squared_butterworth=True):
+    """Create a N-dimensional Butterworth mask for an FFT
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of the n-dimensional FFT and mask.
+    factor : float
+        Fraction of mask dimensions where the cutoff should be.
+    order : float
+        Controls the slope in the cutoff region.
+    high_pass : bool
+        Whether the filter is high pass (low frequencies attenuated) or
+        low pass (high frequencies are attenuated).
+    real : bool
+        Whether the FFT is of a real (True) or complex (False) image
+    squared_butterworth : bool, optional
+        When True, the square of the Butterworth filter is used.
+    Returns
+    -------
+    wfilt : ndarray
+        The FFT mask.
+    """
+    ranges = []
+    for i, d in enumerate(shape):
+        # start and stop ensures center of mask aligns with center of FFT
+        # axis = np.arange(-(d - 1) // 2, (d - 1) // 2 + 1) / (d * factor)
+        axis = np.arange(-(d - 1) / 2, (d - 1) / 2 + 0.5) / (d * factor)  ## FIXED, RH 2023
+        ranges.append(scipy.fft.ifftshift(axis ** 2))
+    # for real image FFT, halve the last axis
+    if real:
+        limit = d // 2 + 1
+        ranges[-1] = ranges[-1][:limit]
+    # q2 = squared Euclidean distance grid
+    q2 = functools.reduce(
+            np.add, np.meshgrid(*ranges, indexing="ij", sparse=True)
+            )
+    q2 = q2.astype(dtype)
+    q2 = np.power(q2, order)
+    wfilt = 1 / (1 + q2)
+    if high_pass:
+        wfilt *= q2
+    if not squared_butterworth:
+        np.sqrt(wfilt, out=wfilt)
+    return wfilt
+
