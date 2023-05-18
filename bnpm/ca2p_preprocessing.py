5a5a90bb6ddb7676af5c062307d51ec4760a85f8
RichieHakim <RichHakim@gmail.com>
2023-05-18 16:21:08 -0400

    fixed default thresh dictionary

--- bnpm/ca2p_preprocessing.py
+++ bnpm/ca2p_preprocessing.py
@@ -468,14 +468,14 @@ def trace_quality_metrics(
         'baseline_var': baseline_var.cpu().numpy().squeeze(),
     }
     thresh = {
-        'var_ratio__Fneu_over_F': 0.5,
-        'EV__F_by_Fneu': 0.5,
-        'base_FneuSub': 75,
-        'base_F': 200,
-        'nsr_autoregressive': 6,
-        'noise_derivMAD': 0.03,
-        'max_dFoF': 10,
-        'baseline_var': 0.01,
+        'var_ratio__Fneu_over_F': (0, 0.5),
+        'EV__F_by_Fneu': (0, 0.7),
+        'base_FneuSub': (100, 2000),
+        'base_F': (200, 3500),
+        'nsr_autoregressive': (0, 6),
+        'noise_derivMAD': (0, 0.015),
+        'max_dFoF': (0.75, 10),
+        'baseline_var': (0, 0.015),
     } if thresh is None else thresh
 
     # Exclude ROIs
