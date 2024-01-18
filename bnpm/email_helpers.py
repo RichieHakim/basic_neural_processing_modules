299fa3f215868fe47c46a1f7da6baeb2db7ed58a
RichieHakim <RichHakim@gmail.com>
2024-01-17 19:21:28 -0500

    Refactor attachment handling in Sender class

--- bnpm/email_helpers.py
+++ bnpm/email_helpers.py
@@ -51,10 +51,12 @@ class Sender():
                 The HTML content of the email.
                 If None, then the ``content`` is assumed to be plain text.
             attachments (list of tuple):
-                A list of tuples containing the file content, file name, and file type.
-                Format: [(file_content, file_name, file_type),].
-                Example of a PDF attachment: [(base64.b64encode(pdf_file.read()).decode(), 'file.pdf', 'application/pdf'),)]
-                Example of a PNG attachment: [(base64.b64encode(png_file.read()).decode(), 'file.png', 'image/png'),)]
+                Either a list of filepaths or a list of tuples containing the file content, file name, and file type.
+                If a list of filepaths, then the file content, file name, and file type will be inferred.
+                If a list of tuples, then:
+                 Format: [(file_content, file_name, file_type), ].
+                 Example of a PDF attachment: [(base64.b64encode(pdf_file.read()).decode(), 'file_name.pdf', 'application/pdf'), ]
+                 Example of a PNG attachment: [(base64.b64encode(png_file.read()).decode(), 'file_name.png', 'image/png'), ]
             verbose (bool):
                 Whether to print the response from SendGrid.
                 If None, then must be defined in the __init__ method.
@@ -75,12 +77,21 @@ class Sender():
         )
 
         if attachments is not None:
+            if isinstance(attachments, list) is False:
+                attachments = [attachments,]
+            if isinstance(attachments[0], str):
+                attachments = [_prepare_attachment(attachment) for attachment in attachments]
+            elif isinstance(attachments[0], tuple):
+                pass
+            else:
+                raise TypeError("Attachments must be a list of filepaths or a list of tuples containing the file content, file name, and file type.")
             for attachment in attachments:
                 message.attachment = Attachment(
                     FileContent(attachment[0]), 
                     FileName(attachment[1]), 
                     FileType(attachment[2]), 
                     Disposition('attachment'), 
+                    ContentId('Attachment')
                 )
 
         response = self.sg.client.mail.send.post(request_body=message.get())
@@ -95,3 +106,31 @@ class Sender():
             print(response.headers)
 
         return response
+
+def _prepare_attachment(file_path):
+    """
+    Prepare a file to be attached to an email.
+    RH 2022
+
+    Args:
+        file_path (str):
+            Path to the file to be attached.
+
+    Returns:
+        tuple:
+            file_content (str):
+                The file content.
+            file_name (str):
+                The file name.
+            file_type (str):
+                The file type.
+    """
+    import base64
+    import mimetypes
+
+    file_name = file_path.split('/')[-1]
+    file_type = mimetypes.guess_type(file_path)[0]
+    with open(file_path, 'rb') as f:
+        file_content = base64.b64encode(f.read()).decode()
+
+    return file_content, file_name, file_type
